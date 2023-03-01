d06a5e5d54dcdb079768f796e800b07b3a0e7734
Rohit-lakhinana-au49 <rohithitler101@gmail.com>
2023-03-01 14:27:16 +0530

    clock

---
+++ clock1/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+//install path
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/clock", function(req,res){
+    res.sendFile("/clock.html",{
+        root: path.join(__dirname, "public")
+    })
+})
+
+app.listen(8000, function(){
+    console.log("server is started with the port number 8000")
+});
+
+//URL'S TO RUN THE HTML FILES
+//http://localhost:8000/
+//http://localhost:8000/about
+//http://localhost:8000/contact
